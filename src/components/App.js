5fea6b6f9108cd5c9fd022ec71a016091babdcb9
Jayson Daquer <me@jaysondaquer.com>
2017-02-23 19:58:35 +0800

    ongoing

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
-// import Header from './common/Header';/
-import {connect} from 'react-redux';
+import Header from './common/Header';
+// import {connect} from 'react-redux';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        {/*<Header*/}
+        <Header/>
           {/*loading={this.props.loading}*/}
-        {/*/>*/}
+
         {this.props.children}
       </div>
     );
@@ -21,10 +21,10 @@ App.propTypes = {
   loading: PropTypes.bool.isRequired
 };
 
-function mapStateToProps(state, ownProps) {
-  return {
-    loading: state.ajaxCallsInProgress > 0
-  };
-}
+// function mapStateToProps(state, ownProps) {
+//   return {
+//     loading: state.ajaxCallsInProgress > 0
+//   };
+// }
 
-export default connect(mapStateToProps)(App);
+export default App;
