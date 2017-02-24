f7776ce87b0c2816ad40a9752387e911b2ada72c
Jayson Daquer <me@jaysondaquer.com>
2017-02-24 21:45:15 +0800

    ongoing

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
+
 // import {connect} from 'react-redux';
 
 class App extends React.Component {
@@ -17,8 +18,8 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  children: PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
+  children: PropTypes.object.isRequired
+  //loading: PropTypes.bool.isRequired
 };
 
 // function mapStateToProps(state, ownProps) {
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,19 +1,39 @@
 import React, {PropTypes} from 'react';
-// import {connect} from 'react-redux';
+import {connect} from 'react-redux';
 // import {bindActionCreators} from 'redux';
-// import * as courseActions from '../../actions/courseActions';
+import * as courseActions from '../../actions/courseActions';
 // import CourseList from './CourseList';
 // import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
-  // constructor(props, context) {
-  //   super(props, context);
-  //   this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
-  // }
+  constructor(props, context) {
+    super(props, context);
 
-  // courseRow(course, index) {
-  //   return <div key={index}>{course.title}</div>;
-  // }
+    this.state = {
+      course:{
+        title:''
+      }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+
+    // this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+  }
+
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course:course});
+  }
+
+  onClickSave(){
+    this.props.createCourse(this.state.course);
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
 
   // redirectToAddCoursePage() {
   //   browserHistory.push('/course');
@@ -21,10 +41,22 @@ class CoursesPage extends React.Component {
 
   render() {
     // const {courses} = this.props;
-
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h2>Add Course</h2>
+
+        <input type="text"
+        onChange={this.onTitleChange}
+        value={this.state.course.title}/>
+
+
+        <input type="submit"
+               onClick={this.onClickSave}
+               value="Save" />
+
+
         {/*<input type="submit"*/}
                {/*value="Add Course"*/}
                {/*className="btn btn-primary"*/}
@@ -35,22 +67,25 @@ class CoursesPage extends React.Component {
   }
 }
 
-// CoursesPage.propTypes = {
-//   courses: PropTypes.array.isRequired,
-//   actions: PropTypes.object.isRequired
-// };
-//
-// function mapStateToProps(state, ownProps) {
-//   return {
-//     courses: state.courses
-//   };
-// }
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+  createCourse: PropTypes.func.isRequired
+};
 
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     actions: bindActionCreators(courseActions, dispatch)
-//   };
-// }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
-export default CoursesPage;
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    // actions: bindActionCreators(courseActions, dispatch)
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
